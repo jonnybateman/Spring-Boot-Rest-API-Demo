5539e0c0aeb26b2b4e019139a5f4b36a533ca226
Jonathan Bateman <jonnybateman@sky.com>
2023-09-04 20:30:15 +0100

    Development updates

--- rest-api-demo/src/main/java/com/cqueltech/restapi/RestapiApplication.java
+++ rest-api-demo/src/main/java/com/cqueltech/restapi/RestapiApplication.java
@@ -2,12 +2,21 @@ package com.cqueltech.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestapiApplication {
+public class RestapiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiApplication.class, args);
 	}
 
+  /*
+   * We need to override the configure() method to deploy to Tomcat container.
+   */
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(RestapiApplication.class);
+  }
 }
